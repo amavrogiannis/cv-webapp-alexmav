9b3a9bfc9e69a440c596b1ac7a958526d69f1584
amavrogiannis <alex.mavrogiannnis@gmail.com>
2023-11-25 00:59:05 +0000

    adding terraform code

---
+++ infra/envs/prod/modules.tf
@@ -0,0 +1,15 @@
+module "cvalexmav_web" {
+  source = "../../modules/web"
+
+  bucket_name = "cv.alexmav.co.uk"
+
+  enable_website = true
+
+  # Tags
+  service_group = "CV_Website"
+  environment   = "Production"
+
+  providers = {
+    aws.virginia = aws.virginia
+  }
+}
---
+++ infra/modules/web/s3.tf
@@ -0,0 +1,84 @@
+#############################################################
+############## S3 Bucket Config #############################
+#############################################################
+
+locals {
+  tags = {
+    Environment   = "${var.environment}"
+    Service_Group = "${var.service_group}"
+  }
+  s3_origin_id = "Application"
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  tags = local.tags
+}
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Website config within the bucket config.
+resource "aws_s3_bucket_website_configuration" "this" {
+  count = var.enable_website ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Get bucket policy
+resource "aws_s3_bucket_policy" "this" {
+  count = var.enable_website ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this_policy.json
+
+  depends_on = [data.aws_iam_policy_document.this_policy]
+}
+
+data "aws_iam_policy_document" "this_policy" {
+  statement {
+    sid = "PublicReadGetObject"
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.this.iam_arn]
+    }
+  }
+
+  depends_on = [
+    aws_cloudfront_origin_access_identity.this
+  ]
+}
